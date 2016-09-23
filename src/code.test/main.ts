cf338fbda003111f80f26ede53cdf6ed6576a74e
Bibby <bibbynet@gmail.com>
2016-09-23 11:34:28 +0800

    add lodash examples

--- src/code.test/main.ts
+++ src/code.test/main.ts
@@ -1,6 +1,6 @@
 /// <reference path="./../../typings/index.d.ts" />
 import * as assert from "assert";
-import {abc} from "./../code/main"
+import {fn} from "./../code/main"
 
 let prepareToRun = (_self, tag: string) => {
 
@@ -16,23 +16,8 @@ export = function () {
 
     prepareToRun(this, "@abcd");
 
-    let input;
-    this.Given(/^the input is "([^"]*)"$/, function (arg1) {
-        input = arg1;
-    });
-
-    let act;
-    this.When(/^run the method getResult$/, function () {
-
-        var obj = new abc();
-        act = obj.getResult(input)
-
-    });
-
-    this.Then(/^the result is "([^"]*)"$/, function (exp) {
-        
-        assert.equal(act,exp);
-
+    this.Then(/^result$/, function () {
+        fn();
     });
 
 }
--- src/code/main.ts
+++ src/code/main.ts
@@ -1,8 +1,38 @@
-export class abc {
+import * as _ from "lodash";
 
-    getResult(arg: string) {
-        return arg + arg;
-    }
+export var fn = () => {
 
-}
+    let users = [
+        { name: 'barney', age: 36, birthday: new Date(1981, 1, 20) },
+        { name: 'fred', age: 40, birthday: new Date(1933, 4, 3) },
+        { name: 'pebbles', age: 18, birthday: new Date(1990, 5, 10) }
+    ];
+
+
+    //map
+    let userName = _.chain(users)
+        .map(user => user.name)
+        .join(",")
+        .value();
+
+    console.log(userName);
+
+    //where
+    let oldUsers = _.chain(users)
+        .filter(a => a.age >= 36)
+        .value();
+    console.log(oldUsers);
+
+
+    //sort (small to big)
+    let sortUsers = _.chain(users)
+        .sortBy(a => a.birthday)
+        .reverse()
+        .value();
+    console.log(sortUsers)
+
+
+
+
+};
 
