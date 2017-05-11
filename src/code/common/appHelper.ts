d5e4b991512211129f25acbe77a10247da3ea0bc
Bibby <bibbynet@gmail.com>
2017-05-11 12:30:06 +0800

    add a method of random code

--- src/code/common/appHelper.ts
+++ src/code/common/appHelper.ts
@@ -16,4 +16,16 @@ export class AppHelper {
 
     }
 
+    static getRandomCode() {
+        
+        //http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 5; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+
 }
