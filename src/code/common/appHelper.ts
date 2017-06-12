c6279f546fae2a3f90f122d577a581cab691b3a1
Bibby <bibbynet@gmail.com>
2017-06-12 14:16:06 +0800

    add a random method

--- src/code/common/appHelper.ts
+++ src/code/common/appHelper.ts
@@ -2,40 +2,44 @@ let crypto = require('crypto');
 
 export class AppHelper {
 
-    static consoleWrite(title: string, obj: any) {
+  static consoleWrite(title: string, obj: any) {
 
-        console.log(`===========${title}===========`);
-        console.log(obj);
-        console.log(`======================`);
+    console.log(`===========${title}===========`);
+    console.log(obj);
+    console.log(`======================`);
 
-    }
+  }
 
-    static getUUID() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
+  static getUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
 
-    }
+  }
 
-    static getRandomCode() {
-        
-        //http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  static getRandomCode() {
 
-        for (var i = 0; i < 5; i++)
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
+    //http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        return text;
-    }
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    static MD5(str){
+    return text;
+  }
 
-        let md5sum = crypto.createHash('md5');
-        let hash = md5sum.update(str).digest('hex')
-        return hash;
+  static MD5(str) {
 
-    }
+    let md5sum = crypto.createHash('md5');
+    let hash = md5sum.update(str).digest('hex')
+    return hash;
+
+  }
+
+  static getRandomArrayValue(array: string[]) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 
 }
--- src/code/main.ts
+++ src/code/main.ts
@@ -1,9 +1,10 @@
 import { lodashRun } from './lodash/main';
 import { momentRun } from './moment/main';
 import { MD5Run } from './md5/main';
+import { AppHelper } from './common/appHelper';
 
 export let testRun = () => {
-    
+
     //lodashRun();
     //momentRun();
     MD5Run();
