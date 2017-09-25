a1609132319ffa8068b0f11792060581e0e1fdf6
Bibby <bibbynet@gmail.com>
2017-09-26 00:15:08 +0800

    add the second example

--- src/code/rxjsExample.ts
+++ src/code/rxjsExample.ts
@@ -1,33 +1,55 @@
-// import Rx from 'rxjs/Rx';
+// import { Observable, Observer } from 'rxjs/Rx';
+
 import { Observer } from 'rxjs/Observer';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/map';
+import 'rxjs/add/observable/filter';
 
 import { AppHelper } from './common/appHelper';
+
+class MyObserver implements Observer<number> {
+  closed?: boolean;
+  next(value: number) {
+    console.log('next:', value);
+  }
+  error(err: any) {
+    console.log('error', err);
+  }
+  complete() {
+    console.log('complete', 'done');
+  }
+}
 
 export class RxjsExample {
 
   run() {
 
     const numberArr = [1, 3, 5];
-    const subject = Observable.from(numberArr);
 
-    subject.subscribe(new MyObserver());
-    subject.subscribe(new MyObserver());
+    // first example
+    AppHelper.consoleWrite('first example', '');
+    const subject0 = Observable.from(numberArr);
+    subject0.subscribe(new MyObserver());
+    subject0.subscribe(
+      value => console.log('next:', value),
+      err => console.log('error', err),
+      () => console.log('complete', 'done'),
+    );
+
+    // second example
+    AppHelper.consoleWrite('second example', '');
+    const subject1: Observable<number> = Observable.create((os: Observer<number>) => {
+      for (const item of numberArr) {
+        os.next(item);
+      }
+      os.complete();
+    });
+    subject1.subscribe(new MyObserver());
+
 
   }
 
 }
 
-class MyObserver implements Observer<number> {
-  closed?: boolean;
-  next(value: number) {
-    AppHelper.consoleWrite('next:', value);
-  }
-  error(err: any) {
-    AppHelper.consoleWrite('error', err);
-  }
-  complete() {
-    AppHelper.consoleWrite('complete', 'done');
-  }
-}
+
